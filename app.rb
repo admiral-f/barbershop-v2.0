56279b0e9737e3a10987b2aacb8f6381d6ceb5c7
admiral-f <admiral-f@yandex.ru>
2015-11-28 14:22:11 +0300

    adding information to database

--- app.rb
+++ app.rb
@@ -7,13 +7,49 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true, length: { minimum: 3 }
+    validates :phone, presence: true
+    validates :barber, presence: true
+    validates :datestamp, presence: true
+    validates :color, presence: true  
 end
 
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers=Barber.all
+end
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
 
 get '/' do
-	@barbers=Barber.all 
+	 
 	erb :index
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+	
+	@booking = Client.new params[:client]
+	if @booking.valid?
+		@booking.save
+		@answer='We are waiting you'
+	end
+  erb :visit
+end
+
+get '/about' do
+  erb :about
 end
